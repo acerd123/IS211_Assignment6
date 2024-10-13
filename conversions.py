c309b6cc4dad5a9b91bfc001a69ccc4c0f543bde
acerd123 <Anibal.cerda86@spsmail.cuny.edu>
2024-10-13 13:16:18 -0400

    Update conversions.py

--- conversions.py
+++ conversions.py
@@ -9,6 +9,5 @@ def convertCelsiusToKelvin(celsius):
 def convertCelsiusToFahrenheit(celsius):
     """Takes in a float representing a Celsius measurement, and returns that temperature converted into Fahrenheit"""
     fahrenheit = (celsius * 9/5) + 32
-    print(f"DEBUG: {celsius}°C is converted to {fahrenheit}°F")
     
     return fahrenheit
